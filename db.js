cd827b80ec4d5ed603452f97443e5029064beaf5
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2021-05-01 10:31:23 +0100

    added heroku DB

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'tom',
-    password: '123456',
-    database: 'merntodo',
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'be34e68d95fea6',
+    password: '559eaed6',
+    database: 'heroku_9c406cf3bfbf88d',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
