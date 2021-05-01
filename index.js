2ee88e792fbf3dc9e15009932760a739ccb562d7
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2021-05-01 10:11:19 +0100

    added path to serve static assets

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 const app = express();
+const path = require('path');
 
 //middleware
 app.use(cors());
@@ -75,6 +76,16 @@ app.delete('/todos/:id', async (req, res) => {
         console.error(err.message);
     }
 })
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
